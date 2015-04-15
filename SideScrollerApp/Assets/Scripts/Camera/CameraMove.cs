34bad6a2b0c31b8dd6b5633f7c2df62542b939a1
zlittle42 <f2pgames42@gmail.com>
2015-04-15 09:40:44 -0700

    Initial commit

---
+++ SideScrollerApp/Assets/Scripts/Camera/CameraMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+	float speed = 10.0f;
+	float leftBound = -10f;
+	float rightBound = 10f;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var move = new Vector3 (Input.GetAxis ("Horizontal"), 0, 0);
+		transform.position += move * speed * Time.deltaTime;
+
+		if(transform.position.x >= rightBound)
+		{
+			transform.position = new Vector3(rightBound,transform.position.y,transform.position.z);
+			Debug.Log ("bounded");
+		}
+		else if(transform.position.x <= leftBound)
+		{
+			transform.position = new Vector3(leftBound,transform.position.y,transform.position.z);
+			Debug.Log ("bounded");
+		}
+	}
+}
---
+++ SideScrollerApp/Assets/Scripts/Character/unitController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class unitController : MonoBehaviour {
+	public float moveSpeed;
+	public string unitTag;
+	public Rigidbody rb;
+	public Vector3 move;
+
+	// Use this for initialization
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		unitTag = gameObject.tag;
+		Debug.Log(unitTag);
+		if(unitTag == "myUnit")
+		{
+			moveSpeed = 2f;
+		}
+		else if (unitTag == "enemyUnit")
+		{
+			moveSpeed = -2f;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		move = new Vector3(moveSpeed,0,0);
+	}
+	void FixedUpdate () {
+		rb.MovePosition (transform.position + move * Time.deltaTime);
+	}
+}
