3af1694ac81ecc927e8fab58724cf4b0166a25dd
zlittle42 <f2pgames42@gmail.com>
2015-04-17 13:02:46 -0700

    Started character select

--- SideScrollerApp/Assets/Scripts/base script/mySpawner.cs
+++ SideScrollerApp/Assets/Scripts/base script/mySpawner.cs
@@ -2,18 +2,33 @@
 using System.Collections;
 
 public class mySpawner : MonoBehaviour {
-
+	public Transform spawn;
+	public GameObject[] characters;
+	public int[] spawnQueue = new int[8];
+	float timeCount;
 	// Use this for initialization
 	void Start () {
 
 	}
-	
 	// Update is called once per frame
 	void Update () {
-	
+		timeCount += Time.deltaTime;
+		if (timeCount >= 3) 
+		{
+			Instantiate (characters [spawnQueue[0]], spawn.position, spawn.rotation);
+			timeCount = 0;
+		}
 	}
-	public void spawnChar()
+	public void spawnChar(int charToSpawn)
 	{
+		for (int i = 0; i < spawnQueue.Length; i ++) 
+		{
+			if(spawnQueue[i] == 0)
+			{
+				spawnQueue[i] = charToSpawn;
+				break;
 
+			}
+		}
 	}
 }
